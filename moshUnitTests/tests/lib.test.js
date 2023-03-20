8d218bbb51b3a1e4c8d33e65410a179dfcf8540f
thiagoFirsen <thiago.firsen@rethink.dev>
2023-03-20 15:47:44 -0300

    new course

--- moshUnitTests/tests/lib.test.js
+++ moshUnitTests/tests/lib.test.js
@@ -1,3 +1,56 @@
-test("First Test", () => {
-  throw new Error("Algo deu errado");
+const lib = require("../lib");
+
+describe("absolute", () => {
+  it("should return a positive number if input is posite", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+  it("should return a positve number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+  it("should return zero if input is zero", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+});
+
+describe("greet", () => {
+  it("should return the greeting message", () => {
+    const result = lib.greet("Mosh");
+    expect(result).toContain("Mosh");
+    expect(result).toMatch(/Mosh/);
+  });
+});
+
+describe("getCurrencies", () => {
+  it("should return supported currencies", () => {
+    const result = lib.getCurrencies();
+    expect(result).toEqual(expect.arrayContaining(["USD", "AUD", "EUR"]));
+  });
+});
+
+describe("getProduct", () => {
+  it("should return the product with the given id", () => {
+    const result = lib.getProduct(1);
+    // expect(result).toMatchObject({ id: 1 });
+    expect(result).toHaveProperty("id", 1);
+  });
+});
+
+describe("registerUser", () => {
+  it("should throw if username is falsy", () => {
+    const args = [null, undefined, NaN, "", 0, false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+
+  it("should return a user object if valid username is passed", () => {
+    const result = lib.registerUser("Mosh");
+    expect(result).toMatchObject({ username: "Mosh" });
+    expect(result.id).toBeGreaterThan(0);
+  });
 });
