9b41a343832cb2754bdd9a02b6d33364a1c91dde
thiagoFirsen <thiago.firsen@rethink.dev>
2023-03-23 10:02:51 -0300

    att

--- jestTypescript/functions.js
+++ jestTypescript/functions.js
@@ -5,11 +5,27 @@ function calculateWageJS(
   overtime = 0,
   clt = true
 ) {
-  if (!workerName || typeof workerName !== "string")
-    throw new Error("Worker name is required and needs to be a string.");
+  if (!workerName || typeof workerName !== "string") {
+    throw new Error("Worker name is required and must be a string");
+  }
+
+  if (
+    typeof hoursWorked !== "number" ||
+    typeof wagePerHour !== "number" ||
+    typeof overtime !== "number"
+  ) {
+    throw new Error("Hours worked, wage per hour and overtime must be numbers");
+  }
+
+  if (hoursWorked < 0 || wagePerHour < 0 || overtime < 0) {
+    throw new Error(
+      "Hours worked, wage per hour and overtime must be positive numbers"
+    );
+  }
+
   const OVERTIME_MULTIPLIER = 2;
   const normalWage = hoursWorked * wagePerHour;
-  const overtimeWage = overtime * OVERTIME_MULTIPLIER;
+  const overtimeWage = overtime * OVERTIME_MULTIPLIER * wagePerHour;
   const finalWage = normalWage + overtimeWage;
 
   return {
@@ -23,7 +39,7 @@ function calculateWageJS(
   };
 }
 
-// console.log(calculateWageJS("Amanda", 2, 160, 20));
+console.log(calculateWageJS("Amanda", 2, 160, 20));
 
 module.exports = {
   calculateWageJS,
--- jestTypescript/functions.test.js
+++ jestTypescript/functions.test.js
@@ -1,26 +1,43 @@
 const fn = require("./functions");
+
 describe("calculateWageJS", () => {
-  it("should return a specific object", () => {
-    expect(fn.calculateWageJS("Amanda", 160, 20, 10)).toMatchObject({
-      workerName: "Amanda",
-      clt: "CLT",
-      hoursWorked: 160,
-      wagePerHour: 20,
-      overtime: 10,
-      overtimeWagePerHour: 40,
-      finalWage: 3220,
-    });
+  const expected = {
+    workerName: "Amanda",
+    clt: "CLT",
+    hoursWorked: 160,
+    wagePerHour: 20,
+    overtime: 10,
+    overtimeWagePerHour: 40,
+    finalWage: 3600,
+  };
+
+  it("should return the expected worker wage object when given valid input", () => {
+    expect(fn.calculateWageJS("Amanda", 160, 20, 10)).toMatchObject(expected);
   });
   it("should return an object", () => {
     const result = fn.calculateWageJS("Amanda", 160, 20, 10);
     expect(typeof result).toBe("object");
   });
-  it("should throw if workername is false or not a string", () => {
+  it("should throw an error when workerName is not a string", () => {
     const args = [null, undefined, NaN, "", 0, false];
     args.forEach((a) => {
       expect(() => {
         fn.calculateWageJS(a, 160, 20, 10);
       }).toThrow();
     });
+  });
+  it("should throw an error when hoursWorked is negative", () => {
+    expect(() => {
+      fn.calculateWageJS("Amanda", -10, 20, 10);
+    }).toThrow();
+  });
+
+  it("should not return NaN or Infinity as the final wage", () => {
+    const result = fn.calculateWageJS("Amanda", Number.MAX_SAFE_INTEGER, 1, 10);
+    expect(isFinite(result.finalWage)).toBe(true);
+  });
+
+  it("should return zero final wage when hours worked is zero", () => {
+    expect(fn.calculateWageJS("Amanda", 0, 20).finalWage).toBe(0);
   });
 });
