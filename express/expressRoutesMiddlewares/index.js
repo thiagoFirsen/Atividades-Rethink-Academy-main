9581e6e69e8faf98361e4f9b11329a0b43dc7364
thiagoFirsen <thiago.firsen@rethink.dev>
2023-04-10 15:54:50 -0300

    10/04

--- express/expressRoutesMiddlewares/index.js
+++ express/expressRoutesMiddlewares/index.js
@@ -3,8 +3,14 @@ import routerItems from "./routes/items.js";
 const items = routerItems;
 import routerHome from "./routes/home.js";
 const home = routerHome;
+
+import multer from "multer";
+const upload = multer();
+
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.any());
 app.use("/", home);
 app.use("/api/items", items);
 
