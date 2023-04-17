3248b632334ac0ae7846082a434b20ffd71c4b6d
thiagoFirsen <thiago.firsen@rethink.dev>
2023-04-17 15:05:13 -0300

    17/04

--- project_fake_store/src/routes/index.ts
+++ project_fake_store/src/routes/index.ts
@@ -1,11 +1,9 @@
 import { router as productsRoutes } from "./products";
 import { router as categoriesRoutes } from "./categories";
-import { router as categoryRoutes } from "./category";
 import { Router } from "express";
 
 const router: Router = Router();
 router.use("/products/categories", categoriesRoutes);
-router.use("/products/category", categoryRoutes);
 router.use("/products", productsRoutes);
 
 export { router };
